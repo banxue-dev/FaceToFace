42ac7e1bfbd527a420f113cf18649a5ab5992389
luojing <a.kiss520@foxmail.com>
2019-10-17 21:34:46 +0800

    JSON修复

--- general-boot/general-common/src/main/java/com/general/redis/FastJsonRedisSerializer.java
+++ general-boot/general-common/src/main/java/com/general/redis/FastJsonRedisSerializer.java
@@ -1,6 +1,7 @@
 package com.general.redis;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
@@ -18,6 +19,10 @@ public class FastJsonRedisSerializer<T> implements RedisSerializer<T> {
     public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
 
     private Class<T> clazz;
+
+    static {
+        ParserConfig.getGlobalInstance().addAccept("com.general");
+    }
 
     public FastJsonRedisSerializer(Class<T> clazz) {
         super();
