daaf7f82ee9813306abbed23757f1ebc37450531
HAOE\fengchaseyou <fengchaseyou@163.com>
2019-12-06 09:22:57 +0800

    增加密码必填

--- general-web/src/views/system/user/form.vue
+++ general-web/src/views/system/user/form.vue
@@ -195,6 +195,10 @@ export default {
           { required: true, message: '请输入登录名', trigger: 'blur' },
           { min: 2, max: 20, message: '长度在 2 到 20 个字符', trigger: 'blur' }
         ],
+		password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 20, message: '长度在 6 到 20 个字符', trigger: 'blur' }
+        ],
         name: [
           { required: true, message: '请输入用户名', trigger: 'blur' },
           { min: 2, max: 20, message: '长度在 2 到 20 个字符', trigger: 'blur' }
